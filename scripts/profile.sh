59215a0d27f347a90993bb725fb83a169d0d355d
sejinik <sejinik@gmail.com>
2020-05-17 15:17:19 +0900

    오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -25,7 +25,7 @@ function find_idle_profile(){
 
   # 쉬고있는 profile의 port 찾기
   function find_idle_port() {
-    IDLE_PROFILE=$(find__find_idle_profile)
+    IDLE_PROFILE=$(find_idle_profile)
 
     if [ ${IDLE_PROFILE} == real1 ]
     then
